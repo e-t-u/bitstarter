5c77e96be4bcd1e438ad3cc9b437abd63ef17d5b
Esa Turtiainen <esa@turtiainen.eu>
2013-07-11 06:56:07 +0300

    corrected ;

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-response.send('Hello World 2!');
+var txt = fs.readFileSync('index.html');
+
+response.send(txt);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
